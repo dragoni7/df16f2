7aec425e42f91a60e54bae1db2029ee0b8587f20
dragoni7 <sdgibson18@outlook.com>
2025-04-27 13:59:06 -0700

    Added header and footer. Style changes for FormNode

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,68 @@
-import { Container } from '@mui/material';
+import { AppBar, Box, Container, Divider, IconButton, Toolbar, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FormGraph from '../features/FormGraph/components/FormGraph';
 import ActionBlueprintGraphProvider from '../features/FormGraph/components/ActionBlueprintGraphProvider';
 
 function App() {
   return (
-    <Container maxWidth="xl" sx={{ height: '100vh' }}>
-      <ActionBlueprintGraphProvider>
-        <FormGraph />
-      </ActionBlueprintGraphProvider>
-    </Container>
+    <>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              DF16F2
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Container maxWidth="xl" sx={{ flexGrow: 1 }}>
+          <ActionBlueprintGraphProvider>
+            <FormGraph />
+          </ActionBlueprintGraphProvider>
+        </Container>
+
+        <Box
+          component="footer"
+          sx={{
+            py: 2,
+            px: 2,
+            backgroundColor: 'lightgray',
+            flexShrink: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 1,
+          }}
+        >
+          <Typography variant="body2" color="text.secondary" align="center">
+            By dragoni7
+          </Typography>
+          <Divider orientation="vertical" flexItem />
+          <IconButton
+            color="inherit"
+            aria-label="GitHub"
+            component="a"
+            href="https://github.com/dragoni7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubIcon />
+          </IconButton>
+          <Divider orientation="vertical" flexItem />
+          <IconButton
+            color="inherit"
+            aria-label="LinkedIn"
+            component="a"
+            href="https://www.linkedin.com/in/samueldgibson/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedInIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </>
   );
 }
 
--- src/features/FormGraph/components/FormNode.tsx
+++ src/features/FormGraph/components/FormNode.tsx
@@ -13,45 +13,49 @@ export default function FormNode({ data }: any) {
   const [formOpen, setFormOpen] = useState<boolean>(false);
 
   return (
-    <>
+    <Paper
+      elevation={1}
+      sx={{
+        padding: 1,
+        display: 'flex',
+        gap: 1,
+        height: '100%',
+        width: '100%',
+        outline: '2px solid rgb(188, 191, 196)',
+        paddingRight: 10,
+        borderRadius: 2,
+      }}
+    >
       <Handle type="target" position={Position.Left} />
-      <Paper
-        elevation={1}
+      <Box
         sx={{
-          padding: 1,
-          display: 'flex',
-          gap: 1,
-          alignItems: 'center',
-          height: '100%',
-          width: '100%',
-          outline: '1px solid grey',
-          paddingRight: 10,
+          backgroundColor: 'rgb(83, 110, 228)',
+          borderRadius: 2,
+          p: 1,
+          color: 'white',
+          '&:hover': {
+            cursor: 'pointer',
+          },
+          alignContent: 'center',
+          textAlign: 'center',
         }}
+        onClick={() => setFormOpen(true)}
       >
-        <Box
-          height="100%"
-          sx={{
-            backgroundColor: 'rgb(72, 100, 224)',
-            borderRadius: 2,
-            p: 1,
-            color: 'white',
-            '&:hover': {
-              cursor: 'pointer',
-            },
-          }}
-          onClick={() => setFormOpen(true)}
-        >
-          <ViewListOutlined />
-        </Box>
-        <Box height="100%">
-          <Typography variant="subtitle2">Form</Typography>
+        <ViewListOutlined />
+      </Box>
+      <Box>
+        <Typography variant="subtitle2" color="textSecondary">
+          Form
+        </Typography>
+
+        <Typography variant="h6">
           <label htmlFor="text">{data.name}</label>
-        </Box>
-      </Paper>
+        </Typography>
+      </Box>
       <Handle type="source" position={Position.Right} />
       <Drawer open={formOpen} onClose={() => setFormOpen(false)} anchor="bottom">
         <Form data={data} />
       </Drawer>
-    </>
+    </Paper>
   );
 }
